7f9115b96e026ac2710fdbe2e99d1e28c1f44d89
Tim Wilson <tim.c.wilson.05@gmail.com>
2019-03-12 20:44:35 -0400

    made it look nicer and improved js

--- todo.js
+++ todo.js
@@ -2,37 +2,43 @@ let input = document.querySelector("input[type = 'text']");
 let ul = document.querySelector("ul")
 let saveBtn = document.querySelector(".save");
 let clearBtn = document.querySelector(".clear");
-let spans = document.getElementsByTagName("span");
+let trashcans = document.getElementsByClassName("delete");
 
 input.addEventListener("keypress", (keyPressed) => {
     if(keyPressed.which === 13){
-        let li = document.createElement("li");
+        let li = document.createElement("li")
+        let listitem = document.createElement("span");
         let spanElement = document.createElement("span");
 
         let newTodo = input.value;
         input.value = "";
 
         spanElement.append("🗑️");
-        ul.appendChild(li).append(spanElement, newTodo);
+        listitem.append(newTodo);
+        spanElement.classList.toggle("delete");
+        listitem.classList.toggle("listitem")
+        ul.appendChild(li).append(spanElement, listitem);
 
         deleteTodo();
     }
 });
 
 const deleteTodo = () => {
-    for(let span of spans){
-        span.addEventListener("click", () => {
-            span.parentElement.remove();
+    for(let icon of trashcans){
+        icon.addEventListener("click", () => {
+            icon.parentElement.remove();
             event.stopPropagation();
         });
     }
 }
 
 ul.addEventListener("click", (ev) => {
-    if (ev.target.tagName === "LI") {
+    if (ev.target.classList.includes("listitem")) {
         ev.target.classList.toggle("checked");
     }
 }, false);
+
+
 
 saveBtn.addEventListener("click", () => {
     localStorage.setItem("todoList", ul.innerHTML);
